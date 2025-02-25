66d61b384f29f5f3811f5b9b8c6e4f96a7d11f22
Angelo Hu <angelohu0402@gmail.com>
2025-02-25 21:34:17 +0000

    Added bigram with Kneser-Ney smoothing

--- main.py
+++ main.py
@@ -100,6 +100,38 @@ class LaplaceUnigramModel:
     def probability(self, word):
         return (self.unigram_counts[word] + 1) / (self.total_words + self.vocab_size)
 
+# kneser-ney smoothing bigram
+class KneserNeyBigramModel:
+    def __init__(self, discount=0.75):
+        self.bigram_counts = defaultdict(int)
+        self.unigram_counts = defaultdict(int)
+        self.continuation_counts = defaultdict(int)
+        self.total_bigrams = 0
+        self.discount = discount
+
+    def train(self, corpus):
+        self.unigram_counts = count_words(corpus)
+        self.bigram_counts = count_bigrams(corpus)
+        self.total_bigrams = sum(self.bigram_counts.values())
+
+        for bigram in self.bigram_counts:
+            self.continuation_counts[bigram[1]] += 1
+    
+    def probability(self, word1, word2):
+        bigram = (word1, word2)
+        unigram_count = self.unigram_counts[word1]
+        bigram_count = self.bigram_counts[bigram]
+        continuation_count = self.continuation_counts[word2]
+        total_unique_bigrams = len(self.bigram_counts)
+
+        if unigram_count > 0:
+            p_continuation = continuation_count / total_unique_bigrams
+            p_bigram = max(bigram_count - self.discount, 0) / unigram_count
+            lambda_w1 = (self.discount / unigram_count) * len([w for w in self.bigram_counts if w[0] == word1])
+            return p_bigram + lambda_w1 * p_continuation
+        else:
+            return 0
+
 # load training data
 train_data = load_data("A1_DATASET/train.txt")
 word_counts = count_words(train_data)
@@ -108,23 +140,31 @@ word_counts = count_words(train_data)
 uniModel = UnigramModel()
 biModel = BigramModel()
 laplaceUniModel = LaplaceUnigramModel()
+KneserNeyBigramModel = KneserNeyBigramModel()
 uniModel.train(train_data)
 biModel.train(train_data)
 laplaceUniModel.train(train_data)
+KneserNeyBigramModel.train(train_data)
 
 # unigram model test
 print("Unigram probability of 'the':", uniModel.probability("the"))
 print("Unigram total words:", uniModel.total_words)
 
 # bigram model test
-print("Bigram probability of 'the':", biModel.probability('the', 'hotel'))
+print("Bigram probability of 'the', 'hotel':", biModel.probability('the', 'hotel'))
 print("Bigram total bigrams:", biModel.total_bigrams)
 
 # laplace smoothing unigram model test
 print("Laplace unigram probability of 'the':", laplaceUniModel.probability("the"))
 print("Laplace total words:", laplaceUniModel.total_words)
 
+# kneser-ney smoothing bigram model test
+print("Kneser-Ney bigram probability of 'the', 'hotel':", KneserNeyBigramModel.probability('the', 'hotel'))
+print("Kneser-Ney total bigrams:", KneserNeyBigramModel.total_bigrams)
+
+# printing output, just uncomment/comment a line to see specific output of a model
 #print(uniModel.unigram_counts)
 #print(biModel.bigram_counts)
-print(laplaceUniModel.unigram_counts)
+#print(laplaceUniModel.unigram_counts)
+#print(KneserNeyBigramModel.bigram_counts)
 #print(sorted(biModel.bigram_counts.items(), key=lambda x: x[1], reverse=True)[:5])
