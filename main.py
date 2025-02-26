0eae91c6b969928a2672fcd59f94bdcaee7f1a40
JT Kim <jefferson.kim2110@gmail.com>
2025-02-26 02:31:38 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # the tokenize part of the NLP 
 from collections import Counter
 from collections import defaultdict
+import math
 import re
 
 def load_data(filepath):
@@ -55,7 +56,6 @@ def count_bigrams(corpus):
     return bigram_count
 
 # unigram model
-#TODO: double check probability, add smoothing
 class UnigramModel:
     def __init__(self):
         self.unigram_counts = {}
@@ -69,7 +69,6 @@ class UnigramModel:
         return self.unigram_counts[word] / self.total_words if self.total_words > 0 else 0
 
 # bigram model
-#TODO: finish this
 class BigramModel:
     def __init__(self):
         self.bigram_counts = defaultdict(int)
@@ -84,6 +83,11 @@ class BigramModel:
     def probability(self, word1, word2):
         bigram = (word1, word2)
         return self.bigram_counts[bigram] / self.unigram_counts[word1] if self.unigram_counts[word1] > 0 else 0
+    
+    def smooth_probability(self, word1, word2, k):
+        bigram = (word1, word2)
+        return (self.bigram_counts[bigram] + k) / (self.unigram_counts[word1] + k * len(self.unigram_counts)) if word1 in self.unigram_counts else k / (k * len(self.unigram_counts))
+        
 
 # laplace smoothing unigram
 class LaplaceUnigramModel:
@@ -132,6 +136,40 @@ class KneserNeyBigramModel:
         else:
             return 0
 
+#Perplexity for unigram
+def unigram_perplexity(model, file):
+    corpus = load_data(file)
+    totalProbabilityLog = 0
+    totalWords = 0
+
+    for i in corpus:
+        sentenceLogProb = 0
+        for j in i:
+            sentenceLogProb += math.log2(model.probability(j))
+        totalProbabilityLog += sentenceLogProb
+        totalWords += len(i)
+    return 2 ** -(totalProbabilityLog / totalWords) if totalWords > 0 else float("inf")
+
+#Perplexity for bigram
+def bigram_perplexity_k(model, file, k):
+    corpus = load_data(file)
+    totalProbabilityLog = 0
+    totalBigrams = 0
+
+    for i in corpus:
+        sentenceLogProb = 0
+        for j in range(len(i) - 1):
+            word1 = i[j]
+            word2 = i[j + 1]
+            totalBigrams += 1
+            sentenceLogProb = model.smooth_probability(word1, word2, k)
+            totalProbabilityLog += math.log2(sentenceLogProb)
+            totalBigrams += 1
+    return 2 ** -(totalProbabilityLog / totalBigrams) if totalBigrams > 0 else float("inf")
+
+
+
+
 # load training data
 train_data = load_data("A1_DATASET/train.txt")
 word_counts = count_words(train_data)
@@ -140,29 +178,50 @@ word_counts = count_words(train_data)
 uniModel = UnigramModel()
 biModel = BigramModel()
 laplaceUniModel = LaplaceUnigramModel()
-KneserNeyBigramModel = KneserNeyBigramModel()
+KneserNeyBiModel = KneserNeyBigramModel()
 uniModel.train(train_data)
 biModel.train(train_data)
 laplaceUniModel.train(train_data)
-KneserNeyBigramModel.train(train_data)
+KneserNeyBiModel.train(train_data)
 
-# unigram model test
+# unigram model probability test
+print("---  PROBABILITY AND WORD COUNT RAW  ---")
 print("Unigram probability of 'the':", uniModel.probability("the"))
 print("Unigram total words:", uniModel.total_words)
 
-# bigram model test
+# bigram model probability test
 print("Bigram probability of 'the', 'hotel':", biModel.probability('the', 'hotel'))
 print("Bigram total bigrams:", biModel.total_bigrams)
+print("\n")
 
-# laplace smoothing unigram model test
+print("---  PROBABILITY AND WORD COUNT SMOOTHED  ---")
+# bigram model probability test with k smoothing
+print("Bigram probability of 'the', 'hotel' with k smoothing:", biModel.smooth_probability('the', 'hotel', 0.1))
+print("Bigram total bigrams:", biModel.total_bigrams)
+
+# laplace smoothing unigram model probability test
 print("Laplace unigram probability of 'the':", laplaceUniModel.probability("the"))
 print("Laplace total words:", laplaceUniModel.total_words)
 
-# kneser-ney smoothing bigram model test
-print("Kneser-Ney bigram probability of 'the', 'hotel':", KneserNeyBigramModel.probability('the', 'hotel'))
-print("Kneser-Ney total bigrams:", KneserNeyBigramModel.total_bigrams)
+# kneser-ney smoothing bigram model probability test
+print("Kneser-Ney bigram probability of 'the', 'hotel':", KneserNeyBiModel.probability('the', 'hotel'))
+print("Kneser-Ney total bigrams:", KneserNeyBiModel.total_bigrams)
+print("\n")
 
-# printing output, just uncomment/comment a line to see specific output of a model
+print("---  PERPLEXITY TEST  ---")
+# unigram perplexity with val.txt
+print("Unigram perplexity of \"val.txt\", laplace smoothed:", unigram_perplexity(laplaceUniModel, "A1_DATASET/val.txt"))
+
+# Bigram perplexity with val.txt
+print("Bigram perplexity of \"val.txt\", k = 0.01 smoothed:", bigram_perplexity_k(biModel, "A1_DATASET/val.txt", 0.01))
+print("Bigram perplexity of \"val.txt\", k = 0.1 smoothed:", bigram_perplexity_k(biModel, "A1_DATASET/val.txt", 0.1))
+print("Bigram perplexity of \"val.txt\", k = 1 smoothed:", bigram_perplexity_k(biModel, "A1_DATASET/val.txt", 1))
+print("Bigram perplexity of \"val.txt\", k = 5 smoothed:", bigram_perplexity_k(biModel, "A1_DATASET/val.txt", 5))
+
+# kneser-ney Bigram perplexity with val.txt //not implemented yet
+#print("")
+
+#printing output, just uncomment/comment a line to see specific output of a model
 #print(uniModel.unigram_counts)
 #print(biModel.bigram_counts)
 #print(laplaceUniModel.unigram_counts)
